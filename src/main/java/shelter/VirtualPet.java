54662df434ea1dec78420a04cd54f1f5d563238b
FilsanMadar <filsanmadar@gmail.com>
2021-04-22 15:32:53 -0400

    start

---
+++ src/main/java/shelter/VirtualPet.java
@@ -0,0 +1,15 @@
+package shelter;
+
+public class VirtualPet {
+
+    private String virtualPetBreed;
+    private String cage;
+    private int age;
+
+    public VirtualPet(String virtualPetBreed, String cage, int age) {
+        this.virtualPetBreed = virtualPetBreed;
+        this.cage = cage;
+        this.age = age;
+
+    }
+}
--- src/test/java/shelter/VirtualPetShelterTest.java
+++ src/test/java/shelter/VirtualPetShelterTest.java
@@ -1,4 +1,29 @@
 package shelter;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class VirtualPetShelterTest {
+
+    VirtualPetShelter underTest;
+    VirtualPet Bulma;
+    VirtualPet Arasu;
+    VirtualPet Blue;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new VirtualPetShelter();
+        Bulma = new VirtualPet("Russian Blue Cat", " Cage 1", 2);
+        Arasu = new VirtualPet("Black Lab", "Cage 2", 2);
+        Blue = new VirtualPet("Horse", "Cage 3", 2);
+    }
+
+    @Test
+    public void shouldAdmitPetToTheShelter(){
+        underTest.
+
+    }
+
+
+
 }
